7ed88509a6a8e6374ea1fb5dcb772fbfef9f8af0
HectorHernandez19 <hectorgustavo150@hotmail.com>
2021-03-03 19:40:13 -0600

    Subi la tarea a github

---
+++ uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,7 @@
+package uaslp.enginering.labs.list;
+
+public class LinkedList {
+    private Node front;
+    private Node tail;
+    private int size;
+}
---
+++ uaslp/enginering/labs/list/Node.java
@@ -0,0 +1,23 @@
+package uaslp.enginering.labs.list;
+
+public class Node {
+    private Node previous;
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    private Node next;
+}
